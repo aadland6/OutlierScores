8b7878197fc004d02fb9f986ef0d075aaf9c8ae2
Matt Aadland <aadland6@users.noreply.github.com>
2017-10-18 15:14:38 -0400

    added impute function

--- missing_values.py
+++ missing_values.py
@@ -5,6 +5,7 @@ Created on Wed Oct 18 10:06:50 2017
 @author: maadland
 """
 import pandas as pd 
+import numpy as np
 
 def complete_cases(df):
     """Counts the compelete cases for an entire data frame 
@@ -38,25 +39,41 @@ def column_completeness_category(df, category_col):
         category_df = df[df[category_col] == category]
         category_column_dict[category] = column_completeness(category_df)
     return category_column_dict
-# Complete cases
-# Missing Data Per Column 
-# Missing Data Per Column Per Category 
-# Correlations between Columns
-# Correlations between Columns Per Category 
+
+def column_median_category(df, category_col):
+    """Computes the median value for the category
+    """
+    category_median_dict = {}
+    for category in set(df[category_col]):
+        category_df = df[df[category_col] == category]
+        category_medians = {}
+        for column in category_df.columns:
+            try:
+                category_medians[column] = np.nanmedian(category_df[column])
+            except TypeError:
+                category_medians[column] = ""
+        category_median_dict[category] = category_medians
+    return category_median_dict
+
+def impute_median_category(df, category_col):
+    """Imputes the median of the data by substted by the category column
+    """
+    median_dfs = []
+    for category in set(df[category_col]):
+        category_df = df[df[category_col] == category]
+        median_dfs.append(category_df.fillna(category_df.median()))
+    return pd.concat(median_dfs)        
 
 if __name__ == "__main__":
     data = pd.read_csv("FakeCSV.csv")
-    a = complete_cases(data)
-    b = complete_cases_category(data, "FakeCategory")
-    c = column_completeness(data)
-    d = column_completeness_category(data, "FakeCategory")
     #### fuels ####
     fuel_d = pd.read_csv("pqis_d.csv")
     keep_columns =  list(fuel_d.columns)[0:-5]
-    fuel_d_clean = fuel_d[keep_columns] 
-    af = complete_cases(fuel_d_clean)
-    bf = column_completeness(fuel_d_clean)
-    out_bf = pd.DataFrame({"ColumnName":list(bf.keys()),
-                           "Completeness":list(bf.values())})
-    out_bf.to_csv("FuelCompleteness.csv", index=False)
+    no_method = [x for x in keep_columns if "_M" not in x]
+    fuel_d_clean = fuel_d[no_method] 
+    fuel_column_scores = column_completeness(fuel_d_clean)
+    fuel_column_subset = [x[0] for x in fuel_column_scores.items() if x[1] > .69]
+    fuel_d_clean_column_subset = fuel_d_clean[fuel_column_subset]
+    fuel_imputed = impute_median_category(fuel_d_clean, "Fuel")
+    
     
